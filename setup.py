3f54f1329827b4b06b73ad5ce0d07cbffefadc26
Jirtus Sanasam <jirtussanasam@gmail.com>
2025-07-07 00:05:04 +0530

    First Setup commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    
+    # This function reads the requirements from a file and returns them as a list.
+    
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements= [req.replace('\n', '') for req in requirements]
+    
+    # Clean up the requirements list
+    requirements = []
+    
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+setup(
+    name='mlpractice',
+    version='0.1.0',
+    author='Jirtus',
+    author_email='jirtussanasam@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirement.txt'),
+)
